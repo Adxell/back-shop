0445686f15e30c987a4edec3062a2b74d5ad3dff
adxell <adxell02@gmail.com>
2022-10-31 08:21:10 -0500

    create documentation to seed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger'
+
+import { RoleProtected } from '../auth/decorators/role-protected.decorator';
+import { UserRoleGuard } from '../auth/guards/user-role/user-role.guard';
+import { validRoles } from '../auth/interfaces/valid-roles';
 import { SeedService } from './seed.service';
 
-
+@ApiTags('Seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
   
   @Get()
+  @RoleProtected( validRoles.superUser, validRoles.admin)
+  @UseGuards( AuthGuard(), UserRoleGuard )
   executeSeed ( ) {
     return this.seedService.runSeed()
   }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
 import { ProductsService } from './../products/products.service';
 import { initialData } from './data/seed-data';
 
@@ -6,16 +9,44 @@ import { initialData } from './data/seed-data';
 export class SeedService {
 
   constructor(
-    private readonly productService: ProductsService
+    private readonly productService: ProductsService, 
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
   ){
 
   }
   async runSeed() {
-    await this.insertNewProducts()
+    await this.deleteTables()
+    const adminUser = await this.insertUsers()
+
+    await this.insertNewProducts(adminUser)
     return 'SEED execute';
   }
 
-  private async insertNewProducts() { 
+  private async deleteTables() {
+    await this.productService.deleteAllProducts()
+    const queryBuilder = this.userRepository.createQueryBuilder()
+    await queryBuilder
+    .delete()
+    .where({})
+    .execute()
+  }
+
+  private async insertUsers () {
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+
+    seedUsers.forEach( user =>{
+      users.push( this.userRepository.create(user) )
+    })
+
+    const dbUsers = await this.userRepository.save( seedUsers )
+
+    return dbUsers[0]
+  }
+  
+  private async insertNewProducts(user: User) { 
     await this.productService.deleteAllProducts()
 
     const products = initialData.products;
@@ -23,7 +54,7 @@ export class SeedService {
     const insertPromises = [];
 
     products.forEach( product => {
-      insertPromises.push(this.productService.create(product))
+      insertPromises.push(this.productService.create(product, user))
     })
 
     await Promise.all(insertPromises)
