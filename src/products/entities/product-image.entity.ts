0399ce6a1e525c80ebcd7c379528e25f867eed4c
adxell <adxell02@gmail.com>
2022-11-01 17:13:23 -0500

    config apiproperty product-image entity

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,14 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from "./";
 
 @Entity({name: 'products_images'})
 export class ProductImage {
+    
+    @ApiProperty({
+        example: "627",
+        description: "Id image"
+    })
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty({
+        description: "URl image"
+    })
     @Column('text')
     url: string;
-
+    
     @ManyToOne(
         () => Product, 
         product => product.images, 
